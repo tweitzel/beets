ee251044950d0dabb27dabf09d5f130b6d32ec86
Adrian Sampson <adrian@radbox.org>
2016-01-24 23:14:39 -0800

    Replace varargs_callback in duplicates
    Now you just use `-k KEY1 -k KEY2`, which seems more natural to me anyway.

--- beetsplug/duplicates.py
+++ beetsplug/duplicates.py
@@ -20,7 +20,7 @@ from __future__ import division, absolute_import, print_function
 import shlex
 
 from beets.plugins import BeetsPlugin
-from beets.ui import decargs, print_, vararg_callback, Subcommand, UserError
+from beets.ui import decargs, print_, Subcommand, UserError
 from beets.util import command_output, displayable_path, subprocess
 from beets.library import Item, Album
 
@@ -79,10 +79,9 @@ class DuplicatesPlugin(BeetsPlugin):
             help=u'report duplicates only if all attributes are set',
         )
         self._command.parser.add_option(
-            u'-k', u'--keys', dest='keys',
-            action='callback', metavar='KEY1 KEY2',
-            callback=vararg_callback,
-            help=u'report duplicates based on keys',
+            u'-k', u'--key',
+            action='append', metavar='KEY',
+            help='report duplicates based on keys (use multiple times)',
         )
         self._command.parser.add_option(
             u'-M', u'--merge', dest='merge',
@@ -124,6 +123,10 @@ class DuplicatesPlugin(BeetsPlugin):
             tiebreak = self.config['tiebreak'].get(dict)
             strict = self.config['strict'].get(bool)
             tag = self.config['tag'].get(str)
+
+            # Command-line override.
+            if opts.key:
+                keys = opts.key
 
             if album:
                 if not keys:
